172294073be26f35fe2309dc1232ccf1550578cb
Oyelami <oyelamiabdulqudus@gmail.com>
2024-10-10 11:15:51 +0100

    My learning tasks will be here for reference purpose

---
+++ README.md
@@ -0,0 +1,1 @@
+The first qustion is to create a function that taks in objects and return the all and the second is to take in an array and return queried or requested index
---
+++ myTasks.js
@@ -0,0 +1,23 @@
+// Task 1
+function counter1(){
+    const OACounter = {
+        firstname : 'Oyelami',
+        lastname : 'Olalekan',
+        age : 12,
+        email : 'abdul@gmail.com',
+        password : 12345,
+        phonenumber : +2348022213212
+    }
+    return OACounter
+}
+console.log('objectresult: ', counter1());
+
+
+
+function counter2(){
+    const ACounter = [firstname = 'Oyelami', lastname = 'Abdulqudus', age = 13, email = 'lekan@yahoomail.com', password = 90123, phonenumber = +2348012223424]
+    return [ACounter[0], ACounter[1], ACounter[3]]    
+}
+//const result = counter2();
+
+console.log('arrayresult: ', counter2());
